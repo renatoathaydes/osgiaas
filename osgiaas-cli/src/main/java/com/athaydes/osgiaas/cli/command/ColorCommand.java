e3fbf15c97f47d11ba9ea516f2f2ab70ee1c861a
renato <renato@twobo.com>
2016-02-15 23:55:40 +0100

    Improved color command. Allow setting prompt, text or error color, or all at once.

--- osgiaas-cli/src/main/java/com/athaydes/osgiaas/cli/command/ColorCommand.java
+++ osgiaas-cli/src/main/java/com/athaydes/osgiaas/cli/command/ColorCommand.java
@@ -1,15 +1,22 @@
 package com.athaydes.osgiaas.cli.command;
 
 import com.athaydes.osgiaas.api.cli.AnsiColor;
+import com.athaydes.osgiaas.api.cli.Cli;
 import com.athaydes.osgiaas.cli.util.UsesCli;
 import org.apache.felix.shell.Command;
 
+import javax.annotation.Nullable;
 import java.io.PrintStream;
+import java.util.Arrays;
 
 /**
  * Implements the shell color command.
  */
 public class ColorCommand extends UsesCli implements Command {
+
+    private enum ColorTarget {
+        PROMPT, TEXT, ERROR, ALL
+    }
 
     @Override
     public String getName() {
@@ -18,31 +25,69 @@ public class ColorCommand extends UsesCli implements Command {
 
     @Override
     public String getUsage() {
-        return "color <color>";
+        return "color <color> [prompt|text|error]";
     }
 
     @Override
     public String getShortDescription() {
-        return "Change the basic color of the text in the shell";
+        return "Changes the color of text in the shell.\n" +
+                "The first argument is the color to set.\n" +
+                "The second, optional argument, may limit the color change to one of " +
+                "[prompt|text|error].\n" +
+                "If the second argument is omitted, all colors are changed.";
     }
 
     @Override
     public void execute( String line, PrintStream out, PrintStream err ) {
         withCli( cli -> {
             String[] parts = line.split( " " );
-            if ( parts.length == 2 ) {
-                try {
-                    AnsiColor color = AnsiColor.valueOf( parts[ 1 ].toUpperCase() );
-                    cli.setPromptColor( color );
-                } catch ( IllegalArgumentException e ) {
-                    err.println( "Invalid color" );
-                }
+            String[] arguments = Arrays.copyOfRange( parts, 1, parts.length );
+            if ( arguments.length == 1 ) {
+                setColor( err, cli, arguments[ 0 ], null );
+            } else if ( arguments.length == 2 ) {
+                String color = arguments[ 0 ];
+                String target = arguments[ 1 ];
+                setColor( err, cli, color, target );
             } else {
-                err.println( "No color provided" );
-                err.println( "Usage: " + getUsage() );
+                printError( err, "Wrong number of arguments provided." );
             }
         } );
     }
 
+    private void printError( PrintStream err, String message ) {
+        err.println( message );
+        err.println( "Usage: " + getUsage() );
+    }
+
+    private void setColor( PrintStream err, Cli cli, String color,
+                           @Nullable String target ) {
+        AnsiColor ansiColor;
+        try {
+            ansiColor = AnsiColor.valueOf( color.toUpperCase() );
+        } catch ( IllegalArgumentException e ) {
+            printError( err, "Invalid color: " + color );
+            return;
+        }
+
+        ColorTarget colorTarget;
+        try {
+            colorTarget = target == null ?
+                    ColorTarget.ALL :
+                    ColorTarget.valueOf( target.toUpperCase() );
+        } catch ( IllegalArgumentException e ) {
+            printError( err, "Invalid target: " + color );
+            return;
+        }
+
+        if ( colorTarget == ColorTarget.ALL || colorTarget == ColorTarget.TEXT ) {
+            cli.setTextColor( ansiColor );
+        }
+        if ( colorTarget == ColorTarget.ALL || colorTarget == ColorTarget.PROMPT ) {
+            cli.setPromptColor( ansiColor );
+        }
+        if ( colorTarget == ColorTarget.ALL || colorTarget == ColorTarget.ERROR ) {
+            cli.setErrorColor( ansiColor );
+        }
+    }
 
 }
