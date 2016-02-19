2cd9c5935fbb898ee8405ff53c109c8d1512397b
renato <renato@twobo.com>
2016-02-19 01:50:48 +0100

    Added CommandHelper.breakupArguments(). Basic incomplete implementation.

--- osgiaas-cli/src/main/java/com/athaydes/osgiaas/cli/util/CommandHelper.java
+++ osgiaas-cli/src/main/java/com/athaydes/osgiaas/cli/util/CommandHelper.java
@@ -2,14 +2,16 @@ package com.athaydes.osgiaas.cli.util;
 
 import java.io.PrintStream;
 
-/**
- * Created by renato on 18/02/16.
- */
 public class CommandHelper {
 
     public static void printError( PrintStream err, String usage, String message ) {
         err.println( message );
         err.println( "Usage: " + usage );
+    }
+
+    public static String[] breakupArguments( String arguments ) {
+        // TODO handle quoted arguments
+        return arguments.split( " " );
     }
 
 }
---
+++ osgiaas-cli/src/test/groovy/com/athaydes/osgiaas/cli/util/CommandHelperSpec.groovy
@@ -0,0 +1,26 @@
+package com.athaydes.osgiaas.cli.util
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class CommandHelperSpec extends Specification {
+
+    @Unroll
+    def "Arguments can be joined to take quotes into consideration"() {
+        when: 'When the example arguments are joined taking quotes into consideration'
+        def result = CommandHelper.breakupArguments( args )
+
+        then: 'Arguments broken up by spaces are joined together if there were quotes around them'
+        result == expectedResult as String[]
+
+        where:
+        args                  | expectedResult
+        ''                    | [ '' ]
+        'abc'                 | [ 'abc' ]
+        'ab c'                | [ 'ab', 'c' ]
+//        'ab "c"'              | [ 'ab', ' c' ]
+//        'ab " c "'            | [ 'ab', ' c ' ]
+//        'ab " c c" "d" e "f"' | [ 'ab', ' c c', 'd', 'e', 'f' ]
+    }
+
+}
