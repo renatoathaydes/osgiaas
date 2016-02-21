5e50b1da81726c48ef7e2e3a84902b533554cccb
renato <renato@twobo.com>
2016-02-21 22:33:21 +0100

    Improved grep command to accept -B and -A flags

--- osgiaas-cli/src/main/java/com/athaydes/osgiaas/cli/command/GrepCommand.java
+++ osgiaas-cli/src/main/java/com/athaydes/osgiaas/cli/command/GrepCommand.java
@@ -3,9 +3,23 @@ package com.athaydes.osgiaas.cli.command;
 import com.athaydes.osgiaas.cli.util.CommandHelper;
 import org.apache.felix.shell.Command;
 
+import javax.annotation.Nullable;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class GrepCommand implements Command {
+
+    private static final Pattern baPattern = Pattern.compile(
+            "\\s*grep\\s+(-B\\s+(\\d+)\\s+)?(-A\\s+(\\d+)\\s+)?(.+)\\s+(.+)",
+            Pattern.DOTALL );
+
+    private static final Pattern abPattern = Pattern.compile(
+            "\\s*grep\\s+(-A\\s+(\\d+)\\s+)?(-B\\s+(\\d+)\\s+)?(.+)\\s+(.+)",
+            Pattern.DOTALL );
 
     @Override
     public String getName() {
@@ -14,31 +28,157 @@ public class GrepCommand implements Command {
 
     @Override
     public String getUsage() {
-        return "grep <regex> <text-to-search>";
+        return "grep [-B <num>] [-A <num>] <regex> <text-to-search>";
     }
 
     @Override
     public String getShortDescription() {
         return "Shows only input text lines matching a regular expression.\n" +
                 "Because it is not possible to enter multiple lines manually, this command is often used to filter " +
-                "output from other commands via the '|' (pipe) operator.";
+                "output from other commands via the '|' (pipe) operator.\n" +
+                "The grep command accepts the following flags:\n" +
+                "  \n" +
+                "  * -B <num>: num is the number of lines to print before each match.\n" +
+                "  * -A <num>: num is the number of lines to print after each match.";
     }
 
     @Override
     public void execute( String line, PrintStream out, PrintStream err ) {
-        String[] parts = CommandHelper.breakupArguments( line, 3 );
-        if ( parts.length == 3 ) {
-            String regex = ".*" + parts[ 1 ] + ".*";
-            String text = parts[ 2 ];
-            String[] textLines = text.split( "\n" );
-            for (String txtLine : textLines) {
-                if ( txtLine.matches( regex ) ) {
-                    out.println( txtLine );
-                }
+        @Nullable GrepCall grepCall = grepCall( line );
+
+        int limit = getLimit( grepCall );
+
+        String[] parts = CommandHelper.breakupArguments( line, limit );
+
+        if ( limit > 0 && parts.length == limit ) {
+            String regex = parts[ limit - 2 ];
+            String text = parts[ limit - 1 ];
+            try {
+                grep( regex, text, grepCall ).forEach( out::println );
+            } catch ( PatternSyntaxException e ) {
+                err.println( "Pattern syntax error in [" + regex + "]: " + e.getMessage() );
             }
         } else {
             CommandHelper.printError( err, getUsage(),
                     "Wrong number of arguments provided." );
         }
     }
+
+    static List<String> grep( String regex, String text,
+                              @Nullable GrepCall grepCall ) {
+        regex = ".*" + regex + ".*";
+        List<String> result = new ArrayList<>();
+        String[] textLines = text.split( "\n" );
+
+        // large number that can be safely added to without overflow
+        int pastLastMatch = 1 << 30;
+        int latestAddedIndex = -1;
+        int beforeLines = grepCall == null ? 0 : grepCall.beforeLines;
+        int afterLines = grepCall == null ? 0 : grepCall.afterLines;
+
+        //noinspection ForLoopReplaceableByForEach
+        for (int i = 0; i < textLines.length; i++) {
+            final String txtLine = textLines[ i ];
+            boolean match = txtLine.matches( regex );
+            if ( match ) {
+                pastLastMatch = 0;
+                int indexToAdd = Math.max( 0, Math.max( latestAddedIndex + 1, i - beforeLines ) );
+                while ( indexToAdd < i ) {
+                    result.add( textLines[ indexToAdd ] );
+                    indexToAdd++;
+                }
+            }
+
+            if ( pastLastMatch <= afterLines ) {
+                latestAddedIndex = i;
+                result.add( txtLine );
+            }
+
+            pastLastMatch++;
+        }
+
+        return result;
+    }
+
+    private static int getLimit( @Nullable GrepCall grepCall ) {
+        int limit;
+        if ( grepCall == null ) {
+            limit = -1; // just cause printError to be called
+        } else if ( grepCall.beforeGiven && grepCall.afterGiven ) {
+            limit = 7;
+        } else if ( grepCall.beforeGiven ^ grepCall.afterGiven ) { // either -B or -A given
+            limit = 5;
+        } else { // neither -B nor -A given
+            limit = 3;
+        }
+        return limit;
+    }
+
+    @Nullable
+    static GrepCall grepCall( String line ) {
+        Matcher abMatch = abPattern.matcher( line );
+        Matcher baMatch = baPattern.matcher( line );
+
+        if ( abMatch.matches() && baMatch.matches() ) {
+            String text = abMatch.group( 6 );
+            if ( text == null || text.isEmpty() ) {
+                return null;
+            }
+
+            @Nullable String after;
+            @Nullable String before;
+
+            int abGroupCount = groupCount( abMatch );
+            int baGroupCount = groupCount( baMatch );
+
+            if ( abGroupCount > baGroupCount ) {
+                after = abMatch.group( 2 );
+                before = abMatch.group( 4 );
+            } else {
+                before = baMatch.group( 2 );
+                after = baMatch.group( 4 );
+            }
+
+            int beforeLines = parseInt( before );
+            int afterLines = parseInt( after );
+
+            return new GrepCall(
+                    beforeLines, before != null,
+                    afterLines, after != null );
+        } else {
+            return null;
+        }
+    }
+
+    private static int parseInt( @Nullable String arg ) {
+        return ( arg == null ) ?
+                0 :
+                Integer.parseInt( arg );
+    }
+
+    private static int groupCount( Matcher matcher ) {
+        int result = 0;
+        for (int i = 1; i <= matcher.groupCount(); i++) {
+            if ( matcher.group( i ) != null ) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    static class GrepCall {
+        final int beforeLines;
+        final boolean beforeGiven;
+        final int afterLines;
+        final boolean afterGiven;
+
+        public GrepCall( int beforeLines, boolean beforeGiven,
+                         int afterLines, boolean afterGiven ) {
+            this.beforeLines = beforeLines;
+            this.beforeGiven = beforeGiven;
+            this.afterLines = afterLines;
+            this.afterGiven = afterGiven;
+        }
+    }
+
 }
---
+++ osgiaas-cli/src/test/groovy/com/athaydes/osgiaas/cli/command/GrepCommandSpec.groovy
@@ -0,0 +1,85 @@
+package com.athaydes.osgiaas.cli.command
+
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class GrepCommandSpec extends Specification {
+
+    @Unroll
+    def "GrepCall is created correctly from a command line"() {
+        when: 'Parsing a valid grep call'
+        def result = GrepCommand.grepCall( line )
+
+        then: 'The grep call is recognized as valid'
+        result != null
+
+        and: 'All parameters are interpreted correctly'
+        result.beforeLines == before
+        result.beforeGiven == beforeGiven
+        result.afterLines == after
+        result.afterGiven == afterGiven
+
+        where:
+        line                 | before | after | beforeGiven | afterGiven
+        'grep a b'           | 0      | 0     | false       | false
+        'grep a b c'         | 0      | 0     | false       | false
+        'grep -B 2 a b'      | 2      | 0     | true        | false
+        'grep -B 2 -A 6 a b' | 2      | 6     | true        | true
+        'grep -A 2 a b'      | 0      | 2     | false       | true
+        'grep -A 2 -B 3 a b' | 3      | 2     | true        | true
+        'grep -A b'          | 0      | 0     | false       | false
+        'grep -B b'          | 0      | 0     | false       | false
+        'grep -B -A b'       | 0      | 0     | false       | false
+        'grep -b x'          | 0      | 0     | false       | false
+    }
+
+    @Unroll
+    def "Invalid grep calls are not recognized"() {
+        when: 'An invalid grep call is made'
+        def result = GrepCommand.grepCall( line )
+
+        then: 'The call is not recognized as being valid'
+        result == null
+
+        where:
+        line << [
+                '', 'abc', 'grepme', 'grep', 'grep abc'
+        ]
+    }
+
+    @Unroll
+    def "Can grep text as expected"() {
+        given: 'a GrepCall with beforeLines = #before and afterLines = #after'
+        def grepCall = new GrepCommand.GrepCall( before, true, after, true )
+
+        and: 'A sample text'
+        def text = '''\
+            |abc
+            |def
+            |ghi
+            |abcdefghi'''.stripMargin()
+
+        when: 'We grep using the regex = #regex'
+        def result = GrepCommand.grep( regex, text, grepCall )
+
+        then: 'The selected lines are as expected'
+        result == expectedResult
+
+        where:
+        regex | before | after | expectedResult
+        'x'   | 0      | 0     | [ ]
+        'a'   | 0      | 0     | [ 'abc', 'abcdefghi' ]
+        'b'   | 0      | 0     | [ 'abc', 'abcdefghi' ]
+        'c$'  | 0      | 0     | [ 'abc' ]
+        'd'   | 0      | 0     | [ 'def', 'abcdefghi' ]
+        'i'   | 0      | 0     | [ 'ghi', 'abcdefghi' ]
+        'cd'  | 0      | 0     | [ 'abcdefghi' ]
+        'a'   | 1      | 0     | [ 'abc', 'ghi', 'abcdefghi' ]
+        'a'   | 0      | 1     | [ 'abc', 'def', 'abcdefghi' ]
+        'd'   | 1      | 1     | [ 'abc', 'def', 'ghi', 'abcdefghi' ]
+        'd'   | 2      | 2     | [ 'abc', 'def', 'ghi', 'abcdefghi' ]
+        'c$'  | 2      | 2     | [ 'abc', 'def', 'ghi' ]
+        'cd'  | 2      | 2     | [ 'def', 'ghi', 'abcdefghi' ]
+    }
+
+}
