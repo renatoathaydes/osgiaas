c2bf02de8b3333b9da7a8811d989058c09fa2743
renato <renato@twobo.com>
2016-02-28 15:32:40 +0100

    Created cli-extra project with a new GroovyCommand

---
+++ osgiaas-cli-extra/src/main/groovy/com/athaydes/osgiaas/cli/extra/command/GroovyCommand.groovy
@@ -0,0 +1,37 @@
+package com.athaydes.osgiaas.cli.extra.command
+
+import com.athaydes.osgiaas.api.cli.CommandHelper
+import groovy.transform.CompileStatic
+import org.apache.felix.shell.Command
+
+@CompileStatic
+class GroovyCommand implements Command {
+
+    @Override
+    String getName() { 'groovy' }
+
+    @Override
+    String getUsage() {
+        'groovy <script>'.stripMargin()
+    }
+
+    @Override
+    String getShortDescription() {
+        'Executes a Groovy script.'
+    }
+
+    @Override
+    void execute( String line, PrintStream out, PrintStream err ) {
+        def args = CommandHelper.breakupArguments( line, 2 )
+        if ( args.size() != 2 ) {
+            CommandHelper.printError( err, getUsage(), 'Wrong number of arguments provided.' )
+        } else try {
+            def result = new GroovyShell( new Binding( line: line, out: out, err: err ) )
+                    .evaluate( args[ 1 ] )
+            if ( result != null ) out.println( result )
+        } catch ( Exception e ) {
+            err.println( e )
+        }
+    }
+
+}
---
+++ osgiaas-cli-extra/src/main/osgi/declarativeServices.groovy
@@ -0,0 +1,12 @@
+commonProperties = { ->
+    property( name: 'service.vendor', value: 'com.athaydes' )
+}
+
+component( name: 'groovyCommand', immediate: true ) {
+    commonProperties()
+    implementation( 'class': 'com.athaydes.osgiaas.cli.extra.command.GroovyCommand' )
+    property( name: 'service.description', value: 'Felix Shell Groovy Command' )
+    service {
+        provide( 'interface': 'org.apache.felix.shell.Command' )
+    }
+}
