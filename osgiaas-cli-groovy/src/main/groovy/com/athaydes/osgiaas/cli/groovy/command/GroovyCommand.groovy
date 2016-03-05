de189f2a0e4afac1ce865da33ab80e39a1fd5474
renato <renato@twobo.com>
2016-03-05 22:37:20 +0100

    GroovyCommand supports basic piping.

--- osgiaas-cli-groovy/src/main/groovy/com/athaydes/osgiaas/cli/groovy/command/GroovyCommand.groovy
+++ osgiaas-cli-groovy/src/main/groovy/com/athaydes/osgiaas/cli/groovy/command/GroovyCommand.groovy
@@ -1,11 +1,12 @@
 package com.athaydes.osgiaas.cli.groovy.command
 
 import com.athaydes.osgiaas.api.cli.CommandHelper
+import com.athaydes.osgiaas.api.cli.StreamingCommand
+import com.athaydes.osgiaas.api.stream.LineAccumulatorOutputStream
 import groovy.transform.CompileStatic
-import org.apache.felix.shell.Command
 
 @CompileStatic
-class GroovyCommand implements Command {
+class GroovyCommand implements StreamingCommand {
 
     @Override
     String getName() { 'groovy' }
@@ -21,13 +22,26 @@ class GroovyCommand implements Command {
     }
 
     @Override
+    OutputStream pipe( String line, PrintStream out, PrintStream err ) {
+        new LineAccumulatorOutputStream( { String fullInput ->
+            run( line.trim() - 'groovy', fullInput, out, err )
+        }, out )
+    }
+
+    @Override
     void execute( String line, PrintStream out, PrintStream err ) {
         def args = CommandHelper.breakupArguments( line, 2 )
         if ( args.size() != 2 ) {
             CommandHelper.printError( err, getUsage(), 'Wrong number of arguments provided.' )
-        } else try {
-            def result = new GroovyShell( new Binding( line: line, out: out, err: err ) )
-                    .evaluate( args[ 1 ] )
+        } else {
+            run( args[ 1 ], '', out, err )
+        }
+    }
+
+    private static void run( String script, String input, PrintStream out, PrintStream err ) {
+        try {
+            def result = new GroovyShell( new Binding( input: input, out: out, err: err ) )
+                    .evaluate( script )
             if ( result != null ) out.println( result )
         } catch ( Exception e ) {
             err.println( e )
