bf3e1b6f3095c1839f03fa2aa877b0ef8d6e3d88
renato <renato@twobo.com>
2016-03-30 20:13:48 +0200

    Added more utility methods to CommandInvocation

--- osgiaas-cli/src/main/java/com/athaydes/osgiaas/api/cli/CommandInvocation.java
+++ osgiaas-cli/src/main/java/com/athaydes/osgiaas/api/cli/CommandInvocation.java
@@ -1,5 +1,7 @@
 package com.athaydes.osgiaas.api.cli;
 
+import javax.annotation.Nullable;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -19,6 +21,20 @@ public class CommandInvocation {
 
     public Map<String, List<String>> getArguments() {
         return arguments;
+    }
+
+    @Nullable
+    public String getArgValue( String argument ) {
+        List<String> values = getAllArgValues( argument );
+        if ( values.isEmpty() ) {
+            return null;
+        } else {
+            return values.get( 0 );
+        }
+    }
+
+    public List<String> getAllArgValues( String argument ) {
+        return arguments.getOrDefault( argument, Collections.emptyList() );
     }
 
     public String getUnprocessedInput() {
