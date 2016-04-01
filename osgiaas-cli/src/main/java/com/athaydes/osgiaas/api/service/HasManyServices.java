c3b202e567967f51620a48a8641627ef90d69d70
renato <renato@twobo.com>
2016-04-02 00:47:45 +0200

    HasManyServices allows sorting services

--- osgiaas-cli/src/main/java/com/athaydes/osgiaas/api/service/HasManyServices.java
+++ osgiaas-cli/src/main/java/com/athaydes/osgiaas/api/service/HasManyServices.java
@@ -1,32 +1,39 @@
 package com.athaydes.osgiaas.api.service;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Utility class that can help handle multiple services.
  */
-public class HasManyServices<ServiceType> {
+public abstract class HasManyServices<ServiceType> {
 
-    private final AtomicReference<Set<ServiceType>> services =
-            new AtomicReference<>( new HashSet<>() );
+    private final AtomicReference<List<ServiceType>> services =
+            new AtomicReference<>( Collections.emptyList() );
+
+    protected abstract Comparator<ServiceType> getComparator();
 
     public void addService( ServiceType service ) {
         services.updateAndGet( old -> {
-            old.add( service );
-            return new HashSet<>( old );
+            List<ServiceType> update = new ArrayList<>( old );
+            update.add( service );
+            Collections.sort( update, getComparator() );
+            return update;
         } );
     }
 
     public void removeService( ServiceType service ) {
         services.updateAndGet( old -> {
-            old.remove( service );
-            return new HashSet<>( old );
+            List<ServiceType> update = new ArrayList<>( old );
+            update.remove( service );
+            return update;
         } );
     }
 
-    public Set<ServiceType> getServices() {
+    public List<ServiceType> getServices() {
         return services.get();
     }
 
