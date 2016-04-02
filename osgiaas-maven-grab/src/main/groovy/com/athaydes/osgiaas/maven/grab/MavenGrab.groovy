971363bbb257e3e6c1df56bb8e32defca34f26d9
renato <renato@twobo.com>
2016-04-02 21:21:22 +0200

    Added support for repositories in MavenGrab. Added verbose option. Documented command.

--- osgiaas-maven-grab/src/main/groovy/com/athaydes/osgiaas/maven/grab/MavenGrab.groovy
+++ osgiaas-maven-grab/src/main/groovy/com/athaydes/osgiaas/maven/grab/MavenGrab.groovy
@@ -9,21 +9,44 @@ import org.apache.felix.shell.Command
 class MavenGrab implements Command {
 
     static final String ADD_REPO = '--add-repo'
+    static final String REMOVE_REPO = '--rm-repo'
+    static final String VERBOSE = '-v'
 
-    // TODO implement add-repo
     final ArgsSpec argsSpec = ArgsSpec.builder()
-            .accepts( "--add-repo", false, true, true )
+            .accepts( ADD_REPO, false, true, true )
+            .accepts( REMOVE_REPO, false, true, true )
+            .accepts( VERBOSE )
             .build()
+
+    private final Map<String, String> repositories = [ : ]
 
     @Override
     String getName() { "grab" }
 
     @Override
-    String getUsage() { "grab <artifact-coordinates>" }
+    String getUsage() { "grab <options> <artifact-coordinates> | <command> <arg>" }
 
     @Override
     String getShortDescription() {
-        "Grabs a Maven artifact from a repository"
+        """
+            Grabs a Maven artifact from a repository.
+
+            Artifact coordinates, composed of groupId, artifactId, and version (optionally, with a classifier),
+            should be joined with ':'.
+
+            Example: grab com.google.guava:guava:19.0
+
+            The following options are supported:
+
+              * -v : verbose mode. Prints information about downloads.
+
+            Grab also supports the following commands:
+
+              * $ADD_REPO <repo> : adds a repository to grab artifacts from.
+              * $REMOVE_REPO <repo> : removes a repository.
+
+            Example: grab --add-repo http://repo.spring.io/release
+            """.stripIndent()
     }
 
     @Override
@@ -37,8 +60,11 @@ class MavenGrab implements Command {
                 CommandHelper.printError( err, getUsage(), "Wrong number of arguments" )
             } else {
                 def reposToAdd = argMap[ ADD_REPO ]
+                def reposToRemove = argMap[ REMOVE_REPO ]
                 if ( reposToAdd ) {
-                    addRepo( reposToAdd )
+                    addRepo( reposToAdd, out, err )
+                } else if ( reposToRemove ) {
+                    removeRepo( reposToRemove, out, err )
                 } else if ( rest ) {
                     def grapes = ( System.getProperty( 'grape.root' ) ?:
                             ( System.getProperty( 'user.home' ) + '/.groovy' ) ) + '/grapes'
@@ -48,6 +74,9 @@ class MavenGrab implements Command {
                     if ( !grapesDir.directory ) {
                         grapesDir.mkdirs()
                     }
+
+                    def verbose = argMap.containsKey( VERBOSE ) ? 'true' : 'false'
+                    System.setProperty( 'groovy.grape.report.downloads', verbose )
 
                     grab rest, out, err, grapes
                 } else {
@@ -59,11 +88,49 @@ class MavenGrab implements Command {
         }
     }
 
-    private static void addRepo( List<String> repos ) {
-        println "Adding repos $repos"
+    private void addRepo( List<String> repos, PrintStream out, PrintStream err ) {
+        for ( repo in repos ) {
+            def parts = repo.split( '=' )
+            def repoId = parts[ 0 ]
+            def repoUri = parts[ parts.size() > 1 ? 1 : 0 ]
+
+            try {
+                repositories.put( repoId, new URI( repoUri ).toString() )
+            } catch ( URISyntaxException e ) {
+                err.println( "Unable to add $repoId: $e" )
+            }
+        }
+
+        showRepos out
     }
 
-    private static void grab( String artifact, PrintStream out, PrintStream err, String grapes ) {
+    private void removeRepo( List<String> repos, PrintStream out, PrintStream err ) {
+        for ( repo in repos ) {
+            def removed = repositories.remove( repo )
+            if ( !removed ) {
+                err.println "Repository not found: $repo"
+            }
+        }
+
+        showRepos out
+    }
+
+    private void showRepos( PrintStream out ) {
+        if ( repositories.isEmpty() ) {
+            out.println "No custom repositories configured."
+        } else {
+            out.println "List of repositories:"
+            repositories.each { name, repo -> out.println "  * $name: $repo" }
+        }
+    }
+
+    private String getReposString() {
+        repositories.collect { name, repo ->
+            "@GrabResolver(name='$name', root='$repo')"
+        }.join( '\n' )
+    }
+
+    private void grab( String artifact, PrintStream out, PrintStream err, String grapes ) {
         def parts = artifact.trim().split( ':' )
         if ( parts.size() == 3 || parts.size() == 4 ) {
             def group = parts[ 0 ]
@@ -75,7 +142,7 @@ class MavenGrab implements Command {
                     ( classifier ? ", classifier='$classifier')" : ')' )
 
             try {
-                Eval.me( grabInstruction + ' import java.util.List' )
+                Eval.me( [ getReposString(), grabInstruction, ' import java.util.List' ].join( '\n' ) )
             } catch ( Throwable ignore ) {
                 err.println( "Unable to download artifact: $artifact" )
                 return
