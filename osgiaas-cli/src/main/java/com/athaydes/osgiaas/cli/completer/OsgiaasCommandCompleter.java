80e8a44d2b830a9e71366c9c22835c21f8d3ae67
renato <renato@twobo.com>
2016-04-03 20:16:46 +0200

    CommandCompleter able to complete even commands using pipes and AND operator.

--- osgiaas-cli/src/main/java/com/athaydes/osgiaas/cli/completer/OsgiaasCommandCompleter.java
+++ osgiaas-cli/src/main/java/com/athaydes/osgiaas/cli/completer/OsgiaasCommandCompleter.java
@@ -13,21 +13,54 @@ public class OsgiaasCommandCompleter
 
     @Override
     public int complete( String buffer, int cursor, List<CharSequence> candidates ) {
-        if ( !buffer.contains( " " ) ) {
-            return completeCommand( buffer, cursor, candidates );
+        EffectiveInput effectiveInput = resolveInputForCompletion( buffer, cursor );
+
+        if ( !effectiveInput.input.contains( " " ) ) {
+            return completeCommand( effectiveInput, candidates );
         } else {
             return -1;
         }
     }
 
-    private int completeCommand( String buffer, int cursor, List<CharSequence> candidates ) {
-        withCliProperties( cliProperties -> {
-            String prefix = buffer.substring( 0, cursor );
-            candidates.addAll( Stream.of( cliProperties.availableCommands() )
-                    .filter( cmd -> cmd.startsWith( prefix ) )
-                    .sorted()
-                    .collect( Collectors.toList() ) );
-        } );
-        return 0;
+    private int completeCommand( EffectiveInput effectiveInput, List<CharSequence> candidates ) {
+        withCliProperties( cliProperties -> candidates.addAll(
+                Stream.of( cliProperties.availableCommands() )
+                        .filter( cmd -> cmd.startsWith( effectiveInput.input ) )
+                        .sorted()
+                        .collect( Collectors.toList() ) ) );
+        return effectiveInput.startIndex;
+    }
+
+    private static EffectiveInput resolveInputForCompletion( String buffer, int cursor ) {
+        String prefix = buffer.substring( 0, cursor );
+
+        int breakupIndex = Math.max( prefix.lastIndexOf( '|' ), prefix.lastIndexOf( "&&" ) );
+
+        final String input;
+        final int index;
+
+        if ( breakupIndex > 0 ) {
+            int firstIndex = Math.min( breakupIndex + 1, prefix.length() - 1 );
+            String rawInput = prefix.substring( firstIndex );
+
+            // left-trim the input. We need to keep trailing spaces to know whether to complete
+            input = rawInput.replaceFirst( "\\s+", "" );
+            index = firstIndex + rawInput.length() - input.length();
+        } else {
+            input = prefix;
+            index = 0;
+        }
+
+        return new EffectiveInput( input, index );
+    }
+
+    private static class EffectiveInput {
+        final String input;
+        final int startIndex;
+
+        EffectiveInput( String input, int startIndex ) {
+            this.input = input;
+            this.startIndex = startIndex;
+        }
     }
 }
