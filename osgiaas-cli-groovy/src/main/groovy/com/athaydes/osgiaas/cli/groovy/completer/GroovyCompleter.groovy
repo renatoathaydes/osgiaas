2fcfca3bb3541a90cc782b593396913be8a5c437
renato <renato@twobo.com>
2016-05-07 20:44:45 +0200

    Improved GroovyCompleter to complete more complex expressions

--- osgiaas-cli-groovy/src/main/groovy/com/athaydes/osgiaas/cli/groovy/completer/GroovyCompleter.groovy
+++ osgiaas-cli-groovy/src/main/groovy/com/athaydes/osgiaas/cli/groovy/completer/GroovyCompleter.groovy
@@ -1,6 +1,7 @@
 package com.athaydes.osgiaas.cli.groovy.completer
 
 import com.athaydes.osgiaas.api.cli.CommandCompleter
+import com.athaydes.osgiaas.api.cli.CommandHelper
 import com.athaydes.osgiaas.api.cli.KnowsCommandBeingUsed
 import com.athaydes.osgiaas.api.cli.completer.BaseCompleter
 import com.athaydes.osgiaas.api.cli.completer.CompletionMatcher
@@ -12,7 +13,6 @@ import javax.annotation.Nullable
 import java.lang.reflect.Method
 import java.lang.reflect.Modifier
 import java.util.concurrent.atomic.AtomicReference
-import java.util.regex.Pattern
 
 import static com.athaydes.osgiaas.api.cli.completer.CompletionMatcher.nameMatcher
 import static com.athaydes.osgiaas.cli.groovy.command.GroovyCommand.ADD_PRE_ARG
@@ -57,7 +57,8 @@ class GroovyCompleter implements CommandCompleter {
             int alternativeResult = new DynamicCompleter( vars, knowsCommandBeingUsed )
                     .complete( buffer, cursor, candidates )
             if ( alternativeResult < 0 ) {
-                alternativeResult = new PropertiesCompleter( vars: vars ).complete( buffer, cursor, candidates )
+                alternativeResult = new PropertiesCompleter( vars: vars )
+                        .complete( buffer, cursor, candidates )
             }
 
             if ( alternativeResult >= 0 ) {
@@ -98,57 +99,104 @@ class PropertiesCompleter implements CommandCompleter {
     int complete( String buffer, int cursor, List<CharSequence> candidates ) {
         String input = buffer.substring( 0, cursor )
 
-        int lastDotIndex = input.lastIndexOf( '.' )
-
-        int lastSpaceIndex = input.lastIndexOf( ' ' )
-        if ( lastSpaceIndex > 0 ) {
-            input = input.substring( Math.min( lastSpaceIndex + 1, input.size() ) )
+        if ( !input.contains( '.' ) ) {
+            // no completion
+            return -1
         }
 
-        String[] parts = input.split( Pattern.quote( '.' ) )
-        if ( input.endsWith( '.' ) ) {
-            parts += ''
+        String finalPart = ''
+
+        CommandHelper.breakupArguments( input, { finalPart = it; true }, false, true,
+                codepoint( ' ' ), codepoint( '"' ), codepoint( "'" ) )
+
+        def tokens = finalPart.split( '\\.' ) as LinkedList<String>
+
+        if ( finalPart.endsWith( '.' ) ) {
+            tokens << ''
         }
 
-        if ( parts.size() == 2 ) {
-            String firstPart = parts[ 0 ].trim()
+        Class varType = Object
+        final toComplete = tokens.removeLast()
+        final tokensIterator = tokens.iterator()
 
-            def var = vars[ firstPart ]
+        if ( tokensIterator.hasNext() ) {
+            final token = tokensIterator.next()
 
-            if ( var == null ) {
-                if ( firstPart.isInteger() ) {
-                    var = 0
-                } else if ( firstPart.isFloat() ) {
-                    var = 0f
-                } else if ( firstPart.isLong() ) {
-                    var = 0L
-                } else if ( firstPart.isDouble() ) {
-                    var = 0D
-                } else if ( firstPart.matches( /['"].*['"]/ ) ) {
-                    var = ''
+            // try to evaluate the first token with the existing bindings
+            try {
+                def result = new GroovyShell( new Binding( vars ) ).evaluate( token )
+
+                if ( result ) {
+                    varType = result.class
                 }
-            }
-
-            if ( var != null ) {
-                boolean typeVar = var instanceof Class
-
-                def filter = typeVar ?
-                        this.&isStatic :
-                        this.&nonStatic
-
-                if ( !typeVar ) {
-                    var = var.class
-                }
-
-                candidates.addAll( ( var as Class ).methods.findAll {
-                    filter( it ) && it.name.startsWith( parts[ 1 ] )
-                }*.name )
-
-                return lastDotIndex + 1
+            } catch ( ignore ) {
+                varType = Object
             }
         }
 
-        return -1
+        while ( tokensIterator.hasNext() ) {
+            def token = tokensIterator.next()
+
+            if ( token.contains( '(' ) && token.endsWith( ')' ) ) { // is method call?
+                def methodName = token[ 0..<( token.indexOf( '(' ) ) ]
+                def returnType = returnTypeOf( methodName, varType )
+
+                if ( returnType ) {
+                    varType = returnType
+                    continue // got it
+                }
+            } else { // is property?
+                def field = varType.fields.find { it.name == token }
+
+                if ( field ) {
+                    varType == field.type
+                    continue // got it
+                } else { // try getter
+                    def methodName = 'get' + token.capitalize()
+                    def returnType = returnTypeOf( methodName, varType )
+
+                    if ( returnType ) {
+                        varType = returnType
+                        continue // got it
+                    }
+                }
+            }
+
+            // got here if nothing worked, varType unknown
+            varType = Object
+        }
+
+        if ( varType ) candidates.addAll( ( varType ).methods.findAll(
+                ( varType == Class ) ? this.&isStatic : this.&nonStatic
+        ).collectMany( this.&toCompletion ).findAll {
+            ( it as String ).startsWith( toComplete )
+        }.sort() )
+        else return -1
+
+        return input.findLastIndexOf { it == '.' } + 1
+    }
+
+    private static int codepoint( String s ) {
+        ( s as char ) as int
+    }
+
+    private static List<String> toCompletion( Method method ) {
+        if ( method.name.startsWith( 'get' ) &&
+                method.name != 'get' &&
+                method.parameterCount == 0 ) {
+            return [ method.name + '()', uncapitalizeAscii( method.name - 'get' ) ]
+        }
+        if ( method.parameterCount > 0 ) {
+            return [ method.name + '(' ]
+        } else {
+            return [ method.name + '()' ]
+        }
+    }
+
+    private static uncapitalizeAscii( String word ) {
+        char[] chars = word.toCharArray()
+        chars[ 0 ] += 32
+        new String( chars )
     }
 
     private static boolean isStatic( Method method ) {
@@ -157,6 +205,17 @@ class PropertiesCompleter implements CommandCompleter {
 
     private static boolean nonStatic( Method method ) {
         !( method.modifiers & Modifier.STATIC )
+    }
+
+    @Nullable
+    private static Class returnTypeOf( String method, Class type ) {
+        def methods = type.methods.findAll { it.name == method }
+        if ( methods ) {
+            // simply choose the first matching method, later we can try to choose the right option
+            return methods.first().returnType
+        } else {
+            return null
+        }
     }
 
 }
