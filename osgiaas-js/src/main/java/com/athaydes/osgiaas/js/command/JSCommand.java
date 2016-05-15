c83195cadf8c5c3fa88d2d92b167d9b21455fbf6
renato <renato@twobo.com>
2016-05-15 18:45:46 +0200

    Added streaming support on JS command

--- osgiaas-js/src/main/java/com/athaydes/osgiaas/js/command/JSCommand.java
+++ osgiaas-js/src/main/java/com/athaydes/osgiaas/js/command/JSCommand.java
@@ -1,7 +1,9 @@
 package com.athaydes.osgiaas.js.command;
 
 import com.athaydes.osgiaas.api.cli.CommandHelper;
-import org.apache.felix.shell.Command;
+import com.athaydes.osgiaas.api.cli.StreamingCommand;
+import com.athaydes.osgiaas.api.stream.LineOutputStream;
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
 import org.osgi.service.component.ComponentContext;
 
 import javax.annotation.Nullable;
@@ -10,11 +12,12 @@ import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
-public class JSCommand implements Command {
+public class JSCommand implements StreamingCommand {
 
     private final AtomicReference<ComponentContext> contextRef = new AtomicReference<>();
 
@@ -70,19 +73,53 @@ public class JSCommand implements Command {
             if ( engine == null ) {
                 err.println( "JavaScript engine is not available" );
             } else try {
-                Bindings bindings = engine.getBindings( ScriptContext.GLOBAL_SCOPE );
-                bindings.put( "out", out );
-                bindings.put( "err", err );
-                bindings.put( "ctx", contextRef.get() );
-
-                @Nullable Object result = engine.eval( script );
-
-                if ( result != null ) {
-                    out.println( result );
-                }
+                runScript( out, err, engine, script );
             } catch ( ScriptException e ) {
                 err.println( e.toString() );
             }
         }
     }
+
+    @Override
+    public OutputStream pipe( String line, PrintStream out, PrintStream err ) {
+        String command = line.substring( "js ".length() );
+
+        try {
+            Object result = runScript( out, err, getEngine(), command );
+            if ( result instanceof ScriptObjectMirror ) {
+                ScriptObjectMirror mirror = ( ScriptObjectMirror ) result;
+                if ( mirror.isFunction() ) {
+
+                    //noinspection CollectionAddedToSelf
+                    return new LineOutputStream( l ->
+                            mirror.call( mirror, l ), out );
+                }
+            }
+
+            throw new RuntimeException( "When used in a pipeline, the JS script must return a " +
+                    "function callback that takes one text line of the input at a time.\n" +
+                    "Example: ... | js function(line) { out.println(\"Line: \" + line); }" );
+        } catch ( ScriptException e ) {
+            throw new RuntimeException( e );
+        }
+    }
+
+    @Nullable
+    private Object runScript( PrintStream out, PrintStream err,
+                              ScriptEngine engine, String script )
+            throws ScriptException {
+        Bindings bindings = engine.getBindings( ScriptContext.GLOBAL_SCOPE );
+        bindings.put( "out", out );
+        bindings.put( "err", err );
+        bindings.put( "ctx", contextRef.get() );
+
+        @Nullable Object result = engine.eval( script );
+
+        if ( result != null ) {
+            out.println( result );
+        }
+
+        return result;
+    }
+
 }
