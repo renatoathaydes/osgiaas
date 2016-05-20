9d6d48ac4f53b778cf6d399f3431f3c50a491aab
renato <renato@twobo.com>
2016-05-20 21:50:16 +0200

    Added GrabCompleter

--- osgiaas-cli-grab/src/main/osgi/declarativeServices.groovy
+++ osgiaas-cli-grab/src/main/osgi/declarativeServices.groovy
@@ -10,3 +10,12 @@ component( name: 'grabCommand', immediate: true ) {
         provide( 'interface': 'org.apache.felix.shell.Command' )
     }
 }
+
+component( name: 'grabCompleter', immediate: true ) {
+    commonProperties()
+    implementation( 'class': 'com.athaydes.osgiaas.cli.grab.GrabCompleter' )
+    property( name: 'service.description', value: 'OSGiaaS CLI Grab Completer' )
+    service {
+        provide( 'interface': 'com.athaydes.osgiaas.api.cli.CommandCompleter' )
+    }
+}
