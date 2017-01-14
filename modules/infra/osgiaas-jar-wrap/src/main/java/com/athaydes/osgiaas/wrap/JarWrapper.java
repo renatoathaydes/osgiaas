b79a6ecdd1b22cda457b8ead90d457c295c0c1ca
renato <renato@twobo.com>
2017-01-14 12:28:54 +0100

    Fixed bug in JarWrapper.isBundle(), improved and documented it.

--- modules/infra/osgiaas-jar-wrap/src/main/java/com/athaydes/osgiaas/wrap/JarWrapper.java
+++ modules/infra/osgiaas-jar-wrap/src/main/java/com/athaydes/osgiaas/wrap/JarWrapper.java
@@ -2,6 +2,8 @@ package com.athaydes.osgiaas.wrap;
 
 import aQute.bnd.osgi.Analyzer;
 import aQute.bnd.osgi.Jar;
+import aQute.bnd.version.MavenVersion;
+import aQute.bnd.version.Version;
 
 import javax.annotation.Nullable;
 import java.io.File;
@@ -33,25 +35,70 @@ public class JarWrapper {
         this.jarFile = jarFile;
     }
 
+    /**
+     * Set the destination of the wrapped jar.
+     * <p>
+     * If the destination file already exists, it will be overwritten.
+     * <p>
+     * By default, the destination is the same as the source file, but with the "-osgi.jar" name ending.
+     * <p>
+     * It can be set to null to revert to the default behavior.
+     *
+     * @param destination where to save the generated OSGi bundle
+     */
     public void setDestination( @Nullable File destination ) {
         this.destination = destination;
     }
 
+    /**
+     * Set the import instructions to provide to Bnd while wrapping the jar.
+     * <p>
+     * The default is "*" (ie. import all packages used by the jar).
+     *
+     * @param importInstructions import instructions for Bnd
+     */
     public void setImportInstructions( String importInstructions ) {
         this.importInstructions = importInstructions;
     }
 
+    /**
+     * Set the export instructions to provide to Bnd while wrapping the jar.
+     * <p>
+     * The default is "*" (ie. export all packages contained in the jar).
+     *
+     * @param exportInstructions export instructions for Bnd
+     */
     public void setExportInstructions( String exportInstructions ) {
         this.exportInstructions = exportInstructions;
     }
 
+    /**
+     * Set the artifact name, used both for the "Bundle-SymbolicName" manifest header and for the generated
+     * bundle file name (if the destination file was not set explicitly).
+     *
+     * @param artifactName name of the artifact
+     */
     public void setArtifactName( @Nullable String artifactName ) {
         this.artifactName = artifactName;
     }
 
+    /**
+     * Wrap the file associated with this instance of {@code JarWrapper} if it is not a bundle already.
+     * <p>
+     * If the source file is already a bundle, nothing is done and this method returns the original file.
+     * Notice that this means the returned file is not necessarily the same as the destination file.
+     *
+     * @param version version to give the bundle. If this is not a valid OSGi version, it will be converted to
+     *                a {@link MavenVersion} and translated to a valid OSGi version.
+     * @return the bundle file
+     * @throws Exception if any error occurs while reading the source file or writing the destination bundle.
+     */
     public File wrap( String version ) throws Exception {
         if ( !jarFile.isFile() ) {
             throw new IllegalArgumentException( "Not a file: " + jarFile );
+        }
+        if ( version.trim().isEmpty() ) {
+            throw new IllegalArgumentException( "Version must not be empty" );
         }
 
         try ( ZipFile input = new ZipFile( jarFile, ZipFile.OPEN_READ ) ) {
@@ -66,7 +113,9 @@ public class JarWrapper {
         try ( Jar newJar = new Jar( jarFile ) ) {
             Analyzer analyzer = new Analyzer();
             analyzer.setJar( newJar );
-            analyzer.setBundleVersion( version );
+            analyzer.setBundleVersion( Version.isVersion( version ) ?
+                    Version.parseVersion( version ) :
+                    MavenVersion.parseString( version ).getOSGiVersion() );
             analyzer.setBundleSymbolicName( name );
             analyzer.setImportPackage( importInstructions );
             analyzer.setExportPackage( exportInstructions );
@@ -81,36 +130,62 @@ public class JarWrapper {
         }
     }
 
+    /**
+     * Check if the given file is a OSGi bundle.
+     * <p>
+     * A jar file is considered a OSGi bundle if its manifest contains the mandatory header
+     * {@code Bundle-SymbolicName}.
+     *
+     * @param input file to check
+     * @return true if this file is probably a bundle, false otherwise
+     * @throws IOException if a problem occurs while reading the file
+     */
     public static boolean isBundle( ZipFile input ) throws IOException {
-        @Nullable ZipEntry manifest = null;
+        @Nullable ZipEntry manifestEntry = null;
 
         Enumeration<? extends ZipEntry> entries = input.entries();
 
         while ( entries.hasMoreElements() ) {
             ZipEntry entry = entries.nextElement();
             if ( MANIFEST_NAME.equals( entry.getName() ) ) {
-                manifest = entry;
+                manifestEntry = entry;
                 break;
             }
         }
 
-        return manifest != null &&
-                new Manifest( input.getInputStream( manifest ) )
-                        .getMainAttributes().containsKey( "Bundle-SymbolicName" );
+        return manifestEntry != null &&
+                new Manifest( input.getInputStream( manifestEntry ) )
+                        .getMainAttributes().getValue( "Bundle-SymbolicName" ) != null;
     }
 
     private static File updateManifest( Jar newJar, File bundle, Manifest manifest )
             throws Exception {
-        if ( bundle.exists() ) {
-            if ( !bundle.delete() ) {
-                throw new IOException( "File already exists and could not be deleted" );
-            }
-        }
-
+        verifyDestinationFileCanBeWritten( bundle );
         newJar.setManifest( manifest );
         newJar.write( bundle );
-
         return bundle;
+    }
+
+    private static void verifyDestinationFileCanBeWritten( File file ) throws IOException {
+        if ( file.exists() ) {
+            if ( !file.delete() ) {
+                throw new IOException( "Destination file already exists and could not be overwritten" );
+            }
+        } else {
+            @Nullable File parent = file.getParentFile();
+
+            boolean parentDirsExist =
+                    // file is top-level
+                    parent == null ||
+                            // parent dir exists
+                            parent.isDirectory() ||
+                            // we could create the parent dirs
+                            file.mkdirs();
+
+            if ( !parentDirsExist ) {
+                throw new IOException( "Destination file's parent directory does not exist and could not be created" );
+            }
+        }
     }
 
     private static String subtract( String text, String toSubtract ) {
