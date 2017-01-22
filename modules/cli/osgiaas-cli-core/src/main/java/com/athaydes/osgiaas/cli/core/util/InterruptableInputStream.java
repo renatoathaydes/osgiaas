13d716ac895a472b26a9f295bfd0f997c9229f1f
renato <renato@twobo.com>
2017-01-22 16:40:55 +0100

    Don't synchronize on Interruptable input stream.

--- modules/cli/osgiaas-cli-core/src/main/java/com/athaydes/osgiaas/cli/core/util/InterruptableInputStream.java
+++ modules/cli/osgiaas-cli-core/src/main/java/com/athaydes/osgiaas/cli/core/util/InterruptableInputStream.java
@@ -41,12 +41,12 @@ public class InterruptableInputStream extends InputStream {
     }
 
     @Override
-    public synchronized void mark( int readlimit ) {
+    public void mark( int readlimit ) {
         in.mark( readlimit );
     }
 
     @Override
-    public synchronized void reset() throws IOException {
+    public void reset() throws IOException {
         in.reset();
     }
 
